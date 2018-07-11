12895c8c9fdb5af96afa161c3d54c5a16bc8df18
Garthon <gartpren1@charter.newvisions.org>
2018-07-11 11:23:38 -0400

    commit message

---
+++ conditionals.js
@@ -0,0 +1,16 @@
+const firstArg = process.argv[0];
+console.log(firstArg);
+
+const secondArg = process.argv[1];
+console.log(secondArg);
+
+thirdArg == undefined
+
+if(value == "whatever"){
+    $20 -= food
+    $70 += jeans
+    $140 += sneakers
+}
+else{
+
+}
